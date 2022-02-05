b9d2f0a8f5894e4d30baadb6993a2f7c6e898373
Kayode Oluwasegun <segunkayode00@gmail.com>
2022-02-05 12:55:34 +0100

    add: generate .gitignore function

--- src/cli.ts
+++ src/cli.ts
@@ -1,15 +1,42 @@
 import inquirer from "inquirer";
 
-inquirer
-  .prompt([
+async function selectFromOptions(message: string, choices: string[]) {
+  const { answer } = await inquirer.prompt([
+    {
+      type: "list",
+      name: "answer",
+      message,
+      choices,
+    },
+  ]);
+
+  return answer;
+}
+
+async function alphabetPrompt() {
+  const { answer } = await inquirer.prompt([
     {
       type: "input",
-      name: "filter",
-      validate: function (input) {
-        let done = this.async;
-      },
+      name: "answer",
+      message:
+        "Enter the initial letter of the language, framework, tools e.t.c",
+
+      validate: (input) =>
+        /^[a-zA-Z]$/.test(input) ? true : "Provide a character between a - z",
     },
-  ])
-  .then((answer) => {
-    console.log(answer);
+  ]);
+
+  return answer.toLowerCase();
+}
+
+async function promptForEditorConfig() {
+  const { answer } = await inquirer.prompt({
+    type: "confirm",
+    name: "answer",
+    message: "Do you want to include configuration for you editor?",
   });
+
+  return answer;
+}
+
+export { promptForEditorConfig, alphabetPrompt, selectFromOptions };
---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import { cliOptions, editorOptions } from "./mappings";
+import {
+  alphabetPrompt,
+  selectFromOptions,
+  promptForEditorConfig,
+} from "./cli";
+import generateIgnoreFile from "./generate_ignorefile";
+
+async function main() {
+  const filter = await alphabetPrompt();
+  const generalType = await selectFromOptions(
+    "Select the framework, project or tool type",
+    cliOptions[filter]
+  );
+
+  const shouldIncludeEditorConfig = await promptForEditorConfig();
+
+  if (!shouldIncludeEditorConfig) {
+    return generateIgnoreFile(generalType);
+  }
+  const editorFilter = await alphabetPrompt();
+  const editorType = await selectFromOptions(
+    "Select the Editor type",
+    editorOptions[editorFilter]
+  );
+
+  return generateIgnoreFile(generalType, editorType);
+}
+
+main();
